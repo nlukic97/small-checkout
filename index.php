ecd9992474f9a5a08bee8105c2d8afd095d5752f
nlukic97 <nlukic97@gmail.com>
2020-06-10 15:15:55 +0200

    new

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
             margin: 0 auto;
         }
         container {
-            margin:0 auto;
+            margin:50px auto 10px auto;
             display: flex;
             justify-content: space-around;
         }
@@ -35,8 +35,10 @@
         }
 
         button {
-            margin: 0 auto;
+            margin: 20px auto;
             display: block;
+            height:50px;
+            width:120px;
         }
     </style>
 </head>
@@ -92,7 +94,22 @@
                 <input type="checkbox" id="vegeterianaExtraCheese" name="vegeterianaExtraCheese">
             </div>
         </container>
+        <container>
+            <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name">
+            </div>
+            <div>
+                <label for="address">Address</label>
+                <input type="text" id="address" name="address">
+            </div>
+            <div>
+                <label for="phone">Phone</label>
+                <input type="number" id="phone" name="phone">
+            </div>
 
+
+        </container>
         <button type="submit">Submit</button>
     </form>
 
